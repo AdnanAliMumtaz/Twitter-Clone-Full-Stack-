f6f63aad6a88f88673ff4d1a1244869226fa138c
Adnan Ali <adnanalimumtaz114@gmail.com>
2024-10-22 18:17:59 +0100

    I

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
+
+// import Post from "../../components";
+
+
 
 const HomePage = () => {
+
+  const [feedType, setFeedType] = useState("forYou");
+
   return (
-    <div>
-        HomePage
-    </div>
+    <>
+      <div className='flex-[4_4_0] mr-auto border-r border-r-gray-700 min-h-screen'>
+        <div className='flex w-full border-b border-gray-700'>
+          <div
+            className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
+            onClick={() => setFeedType("forYou")}
+          >
+            For you
+            {feedType === "forYou" && (<div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary'></div>)}
+          
+          </div>
+          <div className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative" 
+          onClick={()=> setFeedType("following")}
+          >
+            Following
+            {feedType === "following" && (
+              <div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary'></div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
+
+
+
+
+
+
+
 
 export default HomePage
