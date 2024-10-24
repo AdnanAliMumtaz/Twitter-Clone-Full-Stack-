8ff33d33a5fd756acff9c2e775d4c0a8e1574547
Adnan Ali <adnanalimumtaz114@gmail.com>
2024-10-24 13:10:29 +0100

    All of the frontend has been done, just need to connect it to the backend now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
+import Notification from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/notification' element={<Notification />} />
+          <Route path='/profile/:username' element={<ProfilePage />} />
         </Routes>
         <RightPanel />
       </div>
