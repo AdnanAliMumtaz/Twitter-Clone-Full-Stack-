0c7f4fe9486f82408d95ddcd3c588389c7776dbe
Adnan Ali <adnanalimumtaz114@gmail.com>
2024-10-27 14:41:17 +0000

    Added created post frontend page, and connected it with backend successfully

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -1,10 +1,9 @@
 import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
-import { IoCloseSharp, IoHandLeft } from "react-icons/io5";
+import { IoCloseSharp } from "react-icons/io5";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
-
 
 const CreatePost = () => {
     const [text, setText] = useState("");
@@ -14,50 +13,38 @@ const CreatePost = () => {
     const { data: authUser } = useQuery({ queryKey: ["authUser"] });
     const queryClient = useQueryClient();
 
-    const isPending = false;
-    const isError = false;
+    const { mutate: createPost, isPending, isError, error } = useMutation({
+        mutationFn: async ({ text, img }) => {
+            try {
+                const res = await fetch("/api/posts/create", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ text, img }),
 
-    const data = {
-        profileImg: "/avatars/boy1.png"
-    }
+                });
 
-    // const { mutate: createPost, isPending, isError, error } = useMutation({
-    //     mutationFn: async ({ text, img }) => {
-    //         try {
-    //             const res = await fetch("/api/posts/create", {
-    //                 method: "POST",
-    //                 headers: {
-    //                     "Content-Type": "application/json"
-    //                 },
-    //                 body: JSON.stringify({ text, img }),
+                const data = await res.json();
+                if (!res.ok) throw new Error(data.error || "Something went wrong!");
 
-    //             });
+                return data;
 
-    //             const data = await res.json();
-    //             if (!res.ok) throw new Error(data.error || "Something went wrong!");
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: () => {
+            setText("");
+            setImg(null);
+            toast.success("Post created successfully!");
+            queryClient.invalidateQueries({ queryKey: ["posts"] });
+        }
+    });
 
-    //             return data;
-
-    //         } catch (error) {
-    //             throw new Error(error);
-    //         }
-    //     },
-    //     onSuccess: () => {
-    //         setText("");
-    //         setImg(null);
-    //         toast.success("Post created successfully!");
-    //         queryClient.invalidateQueries({ queryKey: ["posts"] });
-    //     }
-    // });
-
-    // const handleSubmit = () => {
-    //     e.preventDefault();
-    //     createPost({ text, img });
-    // };
-
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        alert("Post created successfully!");
+        createPost({ text, img });
     };
 
     const handleImgChange = (e) => {
@@ -69,14 +56,14 @@ const CreatePost = () => {
             };
             reader.readAsDataURL(file);
         }
-    }
+    };
 
 
     return (
         <div className="flex p-4 items-start gap-4 border-b border-gray-700">
             <div className="avatar">
                 <div className="w-8 rounded-full">
-                    <img src={data.profileImg || "/avatar-placeholder.png"} />
+                    <img src={authUser.profileImg || "/avatar-placeholder.png"} />
                 </div>
             </div>
             <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
@@ -97,8 +84,7 @@ const CreatePost = () => {
                         />
                         <img src={img} className="w-full mx-auto h-72 object-contain rounded" />
                     </div>
-                )
-                }
+                )}
                 <div className="flex justify-between border-t py-2 border-t-gray-700">
                     <div className="flex gap-1 items-center">
                         <CiImageOn
@@ -112,10 +98,10 @@ const CreatePost = () => {
                         {isPending ? "Posting.." : "Post"}
                     </button>
                 </div>
-                {isError && <div className="text-red-500">Something went wrong!</div>}
+                {isError && <div className="text-red-500">{error.message}</div>}
             </form>
         </div>
     );
-}
+};
 
 export default CreatePost;
